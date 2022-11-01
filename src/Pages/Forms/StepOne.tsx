866df281103cd9b9b20b854595442b0088baf2bb
Roque Barbosa <roquecostajr@outlook.com>
2022-10-31 21:44:08 -0300

    feat: step one form default values

--- src/Pages/Forms/StepOne.tsx
+++ src/Pages/Forms/StepOne.tsx
@@ -3,17 +3,19 @@ import { ErrorMsg, FormRowWrapper, FormTitle, FromWrapper, InputWrapper } from '
 import { Input } from '../../Styles/Components/Input';
 import { ProgressBar1 } from '../Components/ProgressBar1';
 import { Button } from '../../Styles/Components/Button';
-import { useAppDispatch } from '../../hooks';
-import { changeStep } from '../../store/slices/formSlice';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { changeStep, fillPersonalInfo } from '../../store/slices/formSlice';
 import { useForm } from 'react-hook-form';
 
 function StepOne(): JSX.Element {
 
   const dispatch = useAppDispatch()
+  const client = useAppSelector(state => state.formReducer.client)
   const { register, handleSubmit, formState: { errors }, clearErrors } = useForm();
 
   const onSubmit = (data: any) => {
     console.log(data);
+    dispatch(fillPersonalInfo(data));
     dispatch(changeStep(2));
   }
 
@@ -22,16 +24,20 @@ function StepOne(): JSX.Element {
       <ProgressBar1 />
       <FormTitle>Precisamos conhecer você melhor</FormTitle>
       <InputWrapper>
-        <Input placeholder={'Nome completo'} onMouseOver={() => {clearErrors("name")}}{
+        <Input placeholder={'Nome completo'} 
+          defaultValue={client.name || ""}
+          onMouseOver={() => {clearErrors("name")}}{
           ...register("name", {
-            required: "Esse campo é obrigatório" 
+            required: "Esse campo é obrigatório"
           }
         )} />
         {errors && errors.name && (<ErrorMsg>{errors.name.message as string}</ErrorMsg>)}
       </InputWrapper>
       <FormRowWrapper>
         <InputWrapper>
-        <Input placeholder={'CPF'} onMouseOver={() => {clearErrors("cpf")}} {
+        <Input placeholder={'CPF'} 
+          defaultValue={client.cpf || ""}
+          onMouseOver={() => {clearErrors("cpf")}} {
           ...register("cpf", {
             required: "Esse campo é obrigatório" 
           }
@@ -39,7 +45,9 @@ function StepOne(): JSX.Element {
         {errors && errors.cpf && (<ErrorMsg>{errors.cpf.message as string}</ErrorMsg>)}
         </InputWrapper>
         <InputWrapper>
-        <Input placeholder={'Data de Nascimento'} onMouseOver={() => {clearErrors("birthDate")}} {
+        <Input placeholder={'Data de Nascimento'} 
+          defaultValue={client.birthDate || ""}
+          onMouseOver={() => {clearErrors("birthDate")}} {
           ...register("birthDate", {
             required: "Esse campo é obrigatório" 
           }
@@ -48,7 +56,9 @@ function StepOne(): JSX.Element {
         </InputWrapper>
       </FormRowWrapper>
       <InputWrapper>
-        <Input placeholder={'Seu melhor e-mail'} onMouseOver={() => {clearErrors("email")}} {
+        <Input placeholder={'Seu melhor e-mail'} 
+          defaultValue={client.email || ""}
+          onMouseOver={() => {clearErrors("email")}} {
           ...register("email", {
             required: "Esse campo é obrigatório" 
           }
@@ -57,15 +67,19 @@ function StepOne(): JSX.Element {
       </InputWrapper>
       <FormRowWrapper>
       <InputWrapper>
-        <Input placeholder={'Número de celular'} onMouseOver={() => {clearErrors("phone")}} {
-          ...register("phone", {
+        <Input placeholder={'Número de celular'} 
+          defaultValue={client.cellphone || ""}
+          onMouseOver={() => {clearErrors("phone")}} {
+          ...register("cellphone", {
             required: "Esse campo é obrigatório" 
           }
         )} />
-        {errors && errors.phone && (<ErrorMsg>{errors.phone.message as string}</ErrorMsg>)}
+        {errors && errors.cellphone && (<ErrorMsg>{errors.cellphone.message as string}</ErrorMsg>)}
       </InputWrapper>
       <InputWrapper>
-        <Input placeholder={'Sexo'} onMouseOver={() => {clearErrors("sex")}} {
+        <Input placeholder={'Sexo'} 
+          defaultValue={client.sex || ""}
+          onMouseOver={() => {clearErrors("sex")}} {
           ...register("sex", {
             required: "Esse campo é obrigatório" 
           }
@@ -75,7 +89,9 @@ function StepOne(): JSX.Element {
       </FormRowWrapper>
       <FormRowWrapper>
       <InputWrapper>
-        <Input placeholder={'Tipo de documento'} onMouseOver={() => {clearErrors("identificationDocType")}} {
+        <Input placeholder={'Tipo de documento'} 
+          defaultValue={client.identificationDocType || ""}
+          onMouseOver={() => {clearErrors("identificationDocType")}} {
           ...register("identificationDocType", {
             required: "Esse campo é obrigatório" 
           }
@@ -83,7 +99,9 @@ function StepOne(): JSX.Element {
         {errors && errors.identificationDocType && (<ErrorMsg>{errors.identificationDocType.message as string}</ErrorMsg>)}
       </InputWrapper>
       <InputWrapper>
-        <Input placeholder={'Numero do documento'} onMouseOver={() => {clearErrors("identificationDocNumber")}} {
+        <Input placeholder={'Numero do documento'} 
+          defaultValue={client.identificationDocNumber || ""}
+          onMouseOver={() => {clearErrors("identificationDocNumber")}} {
           ...register("identificationDocNumber", {
             required: "Esse campo é obrigatório" 
           }
@@ -93,7 +111,9 @@ function StepOne(): JSX.Element {
       </FormRowWrapper>
       <FormRowWrapper>
       <InputWrapper>
-        <Input placeholder={'Orgão emissor'} onMouseOver={() => {clearErrors("issuer")}} {
+        <Input placeholder={'Orgão emissor'} 
+          defaultValue={client.issuer || ""}
+          onMouseOver={() => {clearErrors("issuer")}} {
           ...register("issuer", {
             required: "Esse campo é obrigatório" 
           }
@@ -101,7 +121,9 @@ function StepOne(): JSX.Element {
         {errors && errors.issuer && (<ErrorMsg>{errors.issuer.message as string}</ErrorMsg>)}
       </InputWrapper>
       <InputWrapper>
-        <Input placeholder={'Data de expedição'} onMouseOver={() => {clearErrors("issueDate")}} {
+        <Input placeholder={'Data de expedição'} 
+          defaultValue={client.issueDate || ""}
+          onMouseOver={() => {clearErrors("issueDate")}} {
           ...register("issueDate", {
             required: "Esse campo é obrigatório" 
           }
